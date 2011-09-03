a92189e479207b1b50e870a0472ee4a4a2259f5c
wattanadev <wattanadev@hotmail.com>
2011-09-03 11:46:03 +0700

    Initial Commit

---
+++ myOSS/myOSSAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  myOSSAppDelegate.h
+//  myOSS
+//
+//  Created by wattana on 9/3/11.
+//  Copyright 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface myOSSAppDelegate : NSObject <UIApplicationDelegate>
+
+@property (nonatomic, retain) IBOutlet UIWindow *window;
+
+@end
